fda658248c85781023cf48ae0024831b59838d07
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2025-07-30 17:55:28 -0500

    Update backend.js

--- worker/backend.js
+++ worker/backend.js
@@ -56,27 +56,10 @@ async function handle(req, cache) {
   }
 }
 
-async function testFetch(req) {
-  const res = await fetch(req, { redirect: "follow" });
-
-  console.log("Status:", res.status);
-  console.log("Type:", res.type); // opaque / cors / basic
-  console.log("Headers:", [...res.headers.entries()]);
-
-  const buffer = await res.arrayBuffer();
-  console.log("Byte length:", buffer.byteLength);
-
-  const text = new TextDecoder().decode(buffer);
-  console.log("Text version:", text);  // In case it's HTML or error message
-}
-  
-  testFetch()
-
 async function decrypt(req) {
-  // await testFetch(req);
   const oldRes = await fetch(req, { redirect: "follow" });
   const enc = await oldRes.text();
-  console.log(enc);
+  console.log(self.pass, self.key);
   const dec = await Webcrypt.decrypt(enc, self.key);
 
   return new Response(dec, {
